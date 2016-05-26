84734e220ae17175c006de203bb8df74141f3bb8
David Dulac <dsdulac@gmail.com>
2016-05-26 11:48:11 -0400

    Updated auctions files to add an auction list as a partial view

--- DeadWax/Controllers/AuctionsController.cs
+++ DeadWax/Controllers/AuctionsController.cs
@@ -13,7 +13,50 @@ namespace DeadWax.Controllers
         // GET: Auctions
         public ActionResult Index()
         {
-            return View();
+            //Load an array of auctions to display on a list page
+            //Todo: Load from database
+            var auctions = new[]
+            {
+                //Instantiate auction objects into the array
+                new Models.Auction()
+                {
+                    Title = "Example Auction",
+                    Description = "This is an example Auction",
+                    StartTime = DateTime.Now,
+                    EndTime = DateTime.Now.AddDays(7),
+                    StartPrice = 1.00m,
+                    CurrentPrice = null,
+                },
+                new Models.Auction()
+                {
+                    Title = "Example Auction #2",
+                    Description = "This is a second Auction",
+                    StartTime = DateTime.Now,
+                    EndTime = DateTime.Now.AddDays(7),
+                    StartPrice = 1.00m,
+                    CurrentPrice = 30m,
+                },
+                new Models.Auction()
+                {
+                    Title = "Example Auction #3",
+                    Description = "This a third Auction",
+                    StartTime = DateTime.Now,
+                    EndTime = DateTime.Now.AddDays(7),
+                    StartPrice = 1.00m,
+                    CurrentPrice = 24m,
+                },
+            };
+
+            //Pass the array out to the view, access with ViewData or ViewBag
+            return View(auctions);
+        }
+
+        //Use TempData to respond to a request from the view
+        public ActionResult TempDataDemo()
+        {
+            TempData["SuccessMessage"] = "The action succeeded!";
+
+            return RedirectToAction("Index");
         }
 
         public ActionResult Auction()
