e3d153347868c41aff01e5c6ed8f556667538f9d
David Dulac <dsdulac@gmail.com>
2016-06-03 13:17:09 -0400

    Added a search controller that has method Auctions taking a string keyword to perform a db search as a async task

---
+++ DeadWax/Controllers/SearchController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DeadWax.Controllers
+{
+    public class SearchController : AsyncController
+    {
+        //Asyncronus operations, for operations greater than 1 second 
+        // to help with scaling for more concurrent users
+        // releases its thread back to the pool until the process is done
+        // Examples long running operations over 1 second, network or I/O intensive operations, when users can cancel a long running operation
+
+        public async Task<ActionResult> Auctions(string keyword)
+        {
+            var auctions = await Task.Run<IEnumerable<Models.Auction>>(
+                () =>
+                {
+                    var db = new Models.AuctionsDataContext();
+                    return db.Auctions.Where(x => x.Title.Contains(keyword)).ToArray();
+                });
+
+            return Json(auctions, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
